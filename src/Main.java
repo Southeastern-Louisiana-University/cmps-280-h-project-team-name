a696a3ba4185599fcd262aada71a7c470c91f0f2
babri <babritto3@gmail.com>
2024-03-19 10:49:47 -0500

    Template

--- src/Main.java
+++ src/Main.java
@@ -1,4 +1,5 @@
 import java.util.Arrays;
+import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
@@ -7,7 +8,8 @@ public class Main {
         Word a = new Word();
         System.out.println(Arrays.toString(a.wordArr(s)));
 
-
+        Scanner input = new Scanner(System.in);
+                String b = input.nextLine();
 
 
     }
