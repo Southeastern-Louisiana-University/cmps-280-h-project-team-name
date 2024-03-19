c5ec06260f35dfa4d9d4ce0b5e11a80af182cff6
babri <babritto3@gmail.com>
2024-03-19 10:41:18 -0500

    Template

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,14 @@
+import java.util.Arrays;
+
 public class Main {
     public static void main(String[] args) {
         System.out.println("\\ \n" + " \\ \n" + "  | \n" + "  |\n" + " ( )\n" + " \\|/\n" + "  |\n" + " / \\");
+        String s = "Hello";
+        Word a = new Word();
+        System.out.println(Arrays.toString(a.wordArr(s)));
+
+
+
+
     }
 }
