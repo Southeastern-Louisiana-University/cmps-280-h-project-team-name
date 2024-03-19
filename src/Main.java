5c6155273b5a9ca4bc0ffeb1496b77d35ba1675b
W0756128 <christopher.pciou-2@selu.edu>
2024-03-19 11:38:56 -0500

    Version 3

--- src/Main.java
+++ src/Main.java
@@ -5,8 +5,12 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("\\ \n" + " \\ \n" + "  | \n" + "  |\n" + " ( )\n" + " \\|/\n" + "  |\n" + " / \\");
         String s = "Hello";
+
         Word a = new Word();
+        arrToString s2 = new arrToString();
+
         System.out.println(Arrays.toString(a.wordArr(s)));
+        System.out.println(s2.arrToString((a.wordArr(s))));
 
         Scanner input = new Scanner(System.in);
                 String b = input.nextLine();
